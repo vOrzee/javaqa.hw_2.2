662dae29f80bc98795281c6bdc9601b9e29b24ee
Roman Leshin <vorzee@gmail.com>
2024-08-11 17:02:48 +0500

    Добавлено приложение для расчета бонусов при пополнении счета. Реализована проверка на превышение порога пополнения, при котором начисляются бонусы. Итоговая сумма на счету клиента и количество бонусных рублей выводятся на экран.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -13,32 +10,13 @@ public class Main {
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
 
-        // Вариант 1
-        Random rand = new Random();
-        int balance = rand.nextInt(1000);
+        int balance = 1000;
         int bonusBound = 1000;
         int ratioBonus = 100;
         int bonus = 0;
         System.out.println("\nТекущий баланс: " + balance);
-        int incomingPayment = rand.nextInt(3000);
+        int incomingPayment = 1234;
         System.out.println("Введите сумму пополнения: " + incomingPayment);
-
-        balance += incomingPayment;
-        if (incomingPayment > bonusBound) {
-            bonus = (incomingPayment / ratioBonus);
-            balance += bonus;
-        }
-
-        System.out.println("Ваш бонус: " + bonus);
-        System.out.println("Ваш счёт: " + balance);
-        bonus = 0;
-
-        // Вариант 2
-        System.out.println("\nТекущий баланс: " + balance);
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите сумму пополнения: ");
-        incomingPayment = (int) scanner.nextDouble();
-        scanner.close();
 
         balance += incomingPayment;
         if (incomingPayment > bonusBound) {
