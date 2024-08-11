54be2a1439e052d598d61b97ca5f29f7aac5b143
Roman Leshin <vorzee@gmail.com>
2024-08-11 15:17:59 +0500

    task 2 v0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+
+        // Вариант 1
+        Random rand = new Random();
+        int balance = rand.nextInt(1000);
+        int bonusBound = 1000;
+        int ratioBonus = 100;
+        int bonus = 0;
+        System.out.println("\nТекущий баланс: " + balance);
+        int incomingPayment = rand.nextInt(3000);
+        System.out.println("Введите сумму пополнения: " + incomingPayment);
+
+        balance += incomingPayment;
+        if (incomingPayment > bonusBound) {
+            bonus = (incomingPayment / ratioBonus);
+            balance += bonus;
+        }
+
+        System.out.println("Ваш бонус: " + bonus);
+        System.out.println("Ваш счёт: " + balance);
+        bonus = 0;
+
+        // Вариант 2
+        System.out.println("\nТекущий баланс: " + balance);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите сумму пополнения: ");
+        incomingPayment = (int) scanner.nextDouble();
+        scanner.close();
+
+        balance += incomingPayment;
+        if (incomingPayment > bonusBound) {
+            bonus = (incomingPayment / ratioBonus);
+            balance += bonus;
+        }
+
+        System.out.println("Ваш бонус: " + bonus);
+        System.out.println("Ваш счёт: " + balance);
     }
 }
